a26040cdb63c78847488cae8e05cc63e4e7e2007
Paulius Kazokas <pauliusleonardas@gmail.com>
2020-06-20 00:36:23 +0300

    small improvements to signup/registration

--- src/main/java/meniu/MeniuVeiksmai.java
+++ src/main/java/meniu/MeniuVeiksmai.java
@@ -13,87 +13,89 @@ public class MeniuVeiksmai {
 
     public static void pagrindinioMeniuPasirinkimai() throws SQLException  {
 
-        MySQL m = new MySQL();
-
-        Scanner choiseScanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         LOGGER.info("Choice: ");
-        String choice = choiseScanner.nextLine();
+        String choice = scanner.nextLine();
 
         switch(choice) {
             case "0":
                 System.exit(0);
-            case "1":  // prisijungimas
-                // TODO: set other ends to redirect
-                Scanner usernameScanner = new Scanner(System.in);
-                LOGGER.info("Enter username: ");
-                String username = usernameScanner.nextLine();
-
-                if(m.checkIfUserExists(m.connectionToDatabase(), username) != null) {
-                    Scanner passwordScanner = new Scanner(System.in);
-                    LOGGER.info("Enter password: ");
-                    String password = passwordScanner.nextLine();
-                    int maximumUnsuccessfullRetries = 4;
-                    int unsuccessfulLogin = 1;
-                    boolean rightPassword = false;
-                    if(m.checkIfPasswordMatches(m.connectionToDatabase(), username, password)) {
-                        LOGGER.info("Welcome, {}", username);
-                        Meniu.pagrindinisPaskyrosMeniu();
-                    } else {
-                        LOGGER.warn("Password is incorrect. ({}) retries left..", (maximumUnsuccessfullRetries - 1));
-                        while(unsuccessfulLogin < maximumUnsuccessfullRetries) {
-                            Scanner unsuccessfulPasswordScanner = new Scanner(System.in);
-                            LOGGER.warn("Try again, enter password: ");
-                            String unsuccessfullPassword = unsuccessfulPasswordScanner.nextLine();
-                            if(m.checkIfPasswordMatches(m.connectionToDatabase(), username, unsuccessfullPassword)) {
-                                unsuccessfulLogin = maximumUnsuccessfullRetries;
-                                rightPassword = true;
-                            } else {
-                                unsuccessfulLogin++;
-                            }
-                        }
-                        if(rightPassword = true) {
-                            LOGGER.info("Welcome, '{}'", username);
-                            Meniu.pagrindinisPaskyrosMeniu();
-                        } else {
-                            LOGGER.error("({}) Maximum retries exceeded..", unsuccessfulLogin);
-                            Meniu.pagrindinisMeniu();
-                        }
-                    }
-                } else {
-                    LOGGER.info("Username doesn't exist, would you like to register an account? (y/n)");
-                    System.exit(0);
-                }
+            case "1":
+                signup(scanner);
                 break;
-            case "2":  // registracija
-
-                Scanner registerUsername = new Scanner(System.in);
-                LOGGER.info("Enter username: ");
-                String regUsername = registerUsername.nextLine();
-
-                Scanner registerPassword = new Scanner(System.in);
-                LOGGER.info("Enter password: ");
-                String regPassword = registerPassword.nextLine();
-
-                Scanner registerName = new Scanner(System.in);
-                LOGGER.info("Enter name: ");
-                String regName = registerName.nextLine();
-
-                Scanner registerLastname = new Scanner(System.in);
-                LOGGER.info("Enter lastname: ");
-                String regLastname = registerLastname.nextLine();
-
-                Scanner registerLEmail = new Scanner(System.in);
-                LOGGER.info("Enter email: ");
-                String regEmail = registerLEmail.nextLine();
-
-                Scanner registerPersonalCode = new Scanner(System.in);
-                LOGGER.info("Enter personal code: ");
-                String regPersonalCode = registerPersonalCode.nextLine();
-
-                m.registerNewUser(m.connectionToDatabase(), regUsername, regPassword, regName, regLastname, regEmail, regPersonalCode);
-
-                LOGGER.info("Registration information: {}, {}, {}, {}, {}, {}", regUsername, regPassword, regName, regLastname, regEmail, regPersonalCode);
+            case "2":
+                registration(scanner);
                 break;
         }
+    }
+
+    public static void signup(Scanner scanner) throws SQLException {
+
+        MySQL m = new MySQL();
+        LOGGER.info("Enter username: ");
+        String username = scanner.nextLine();
+
+        if(m.checkIfUserExists(m.connectionToDatabase(), username) != null) {
+            LOGGER.info("Enter password for {}: ", username);
+            String password = scanner.nextLine();
+            int maximumUnsuccessfullRetries = 4;
+            int unsuccessfulLogin = 1;
+            boolean rightPassword = false;
+            if(m.checkIfPasswordMatches(m.connectionToDatabase(), username, password)) {
+                LOGGER.info("Welcome, {}", username);
+                Meniu.pagrindinisPaskyrosMeniu();
+            } else {
+                LOGGER.warn("Password is incorrect. ({}) retries left..", (maximumUnsuccessfullRetries - 1));
+                while(unsuccessfulLogin < maximumUnsuccessfullRetries) {
+                    LOGGER.warn("Try again, enter password: ");
+                    String unsuccessfullPassword = scanner.nextLine();
+                    if(m.checkIfPasswordMatches(m.connectionToDatabase(), username, unsuccessfullPassword)) {
+                        unsuccessfulLogin = maximumUnsuccessfullRetries;
+                        rightPassword = true;
+                    } else {
+                        unsuccessfulLogin++;
+                    }
+                }
+                if(rightPassword) {
+                    LOGGER.info("Welcome, '{}'", username);
+                    Meniu.pagrindinisPaskyrosMeniu();
+                } else {
+                    LOGGER.error("({}) Maximum retries exceeded..", unsuccessfulLogin);
+                    Meniu.pagrindinisMeniu();
+                }
+            }
+        } else {
+            LOGGER.info("Username doesn't exist, would you like to register an account? (y/n)");
+            if(scanner.nextLine().equals("y") || scanner.nextLine().equals("Y")) {
+
+            }
+            System.exit(0);
+        }
+    }
+
+    public static void registration(Scanner scanner) throws SQLException {
+        LOGGER.info("Enter username: ");
+        String regUsername = scanner.nextLine();
+
+        LOGGER.info("Enter password: ");
+        String regPassword = scanner.nextLine();
+
+        LOGGER.info("Enter name: ");
+        String regName = scanner.nextLine();
+
+        LOGGER.info("Enter lastname: ");
+        String regLastname = scanner.nextLine();
+
+        LOGGER.info("Enter email: ");
+        String regEmail = scanner.nextLine();
+
+        LOGGER.info("Enter personal code: ");
+        String regPersonalCode = scanner.nextLine();
+
+        MySQL m = new MySQL();
+        m.registerNewUser(m.connectionToDatabase(), regUsername, regPassword, regName, regLastname, regEmail, regPersonalCode);
+
+        LOGGER.info("Registration information: {}, {}, {}, {}, {}, {}", regUsername, regPassword, regName, regLastname, regEmail, regPersonalCode);
+        Meniu.pagrindinisMeniu();
     }
 }
